c2fa8475700b95b79b2f719178f11c4593990e9e
karlderkaefer <karlderkaefer@users.noreply.github.com>
2023-05-23 14:16:33 +0200

    feat: add helm plugin

---
+++ scripts/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Make sure k8s-helm-dep-updater is in PATH
+command -v k8s-helm-dep-updater >/dev/null 2>&1 || { echo >&2 "k8s-helm-dep-updater not found. Please install it first."; exit 1; }
+
+k8s-helm-dep-updater >/dev/null
