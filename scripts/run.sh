2edd2e1349e66a92456f71792cb1e7256312a5a7
Michael J. Wieneke <m.wieneke@sap.com>
2024-02-26 16:04:28 +0100

    fix: updating plugin-version and script only requires sh now

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Make sure k8s-helm-dep-updater is in PATH
 command -v k8s-helm-dep-updater >/dev/null 2>&1 || { echo >&2 "k8s-helm-dep-updater not found. Please install it first."; exit 1; }
